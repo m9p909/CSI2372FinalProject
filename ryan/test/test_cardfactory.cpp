ec7ec00a7a92d6040e42930a599fd2bd16aa5455
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-12-06 11:41:32 -0700

    Add more CardFactory unit tests.

--- ryan/test/test_cardfactory.cpp
+++ ryan/test/test_cardfactory.cpp
@@ -9,4 +9,21 @@ TEST_CASE("Test CardFactory")
 
   // The deck should have a size of 104 cards.
   CHECK(y.size() == 104);
+
+  // Count the number of cards in the generated deck, ensure the numbers are correct.
+  // There should be <STINK_CARDS> stink cards in the shuffled deck.
+  unsigned int stink_cards = 0;
+  for (int i = 0; i < 104; i++)
+  {
+    if (y.at(i)->getName() == "Stink")
+    {
+      stink_cards++;
+    }
+  }
+  CHECK(stink_cards == STINK_CARDS);
+
+  // Drawing a card should reduce the count of that card by one.
+  Card *dc = y.draw();
+  string cardTypeName = dc->getName();
+  CHECK(y.size() == 103);
 }
